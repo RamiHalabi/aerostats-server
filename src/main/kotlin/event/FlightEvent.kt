6848d473485e5c61252d2bee0b8fc2949ca90167
Rami halabi <ramiihalabi@outlook.com>
2025-08-23 13:03:54 -0500

    Delete flight events and event bus

--- src/main/kotlin/event/FlightEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.ramihalabi.event
-
-import model.FlightDataModel
-
-sealed class FlightEvent {
-    data class Saved(val flight: FlightDataModel, val isNew: Boolean) : FlightEvent()
-    data class Failed(val flight: FlightDataModel, val exception: Throwable) : FlightEvent()
-}
